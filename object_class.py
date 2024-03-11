a3703c9100a1a75bcaa23dde104eeeb24e8d88da
Honza1024 <janexpert@seznam.cz>
2024-03-12 00:03:12 +0100

    object_class.py and pycharm files added

---
+++ object_class.py
@@ -0,0 +1,7 @@
+class Object:
+
+    def __init__(self, typ, pos, sprite, health = False):
+        self.typ = typ
+        self.pos = pos
+        self.sprite = sprite
+        self.health = health
