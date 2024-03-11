41c2c6053becb3c6e15d9eb275037525d54a5252
Honza1024 <97306428+Honza1024@users.noreply.github.com>
2024-03-11 14:31:09 +0100

    Fixed collisions with walls
    Added teleporting out of current tile if it is a wall

--- player_class.py
+++ player_class.py
@@ -8,7 +8,7 @@ class Player:
         self.direction = dir
         self.fov = 1.2
         self.noDeformation = True
-        self.speed = 1
+        self.speed = 1.5
         self.sensitivity = 0.05
         self.thickness = 0.06
 
@@ -16,7 +16,29 @@ class Player:
     def collide(self, mapArray):
         #if inside a wall, get pushed away
         if mapa.isWall(int(self.position[0]), int(self.position[1]), mapArray):
-            pass #find closest side and move there
+            relativeX = (self.position[0] % 1) - 0.5
+            relativeY = (self.position[1] % 1) - 0.5
+            iteration = 1
+            while True: #find closest free tile
+                freeTiles = []
+                for x in range(-iteration, iteration + 1):
+                    for y in range(-iteration, iteration + 1):
+                        if x == 0 and y == 0:
+                            continue
+                        if not mapa.isWall(int(self.position[0] + x * iteration), int(self.position[1] + y * iteration), mapArray):
+                            freeTiles.append((x, y))
+                if len(freeTiles):
+                    break
+                iteration += 1
+            distances = []
+            for tile in freeTiles:
+                distances.append((self.position[0] - (int(self.position[0]) + tile[0] + 0.5)) ** 2 + (self.position[1] - (int(self.position[1]) + tile[1] + 0.5)) ** 2)
+            closest = freeTiles[distances.index(min(distances))]
+            # teleport to the closest tile
+            if closest[0] != 0:
+                self.position[0] = int(self.position[0]) + closest[0] + (self.thickness if closest[0] > 0 else 1 - self.thickness)
+            if closest[1] != 0:
+                self.position[1] = int(self.position[1]) + closest[1] + (self.thickness if closest[1] > 0 else 1 - self.thickness)
         # keep some distance from walls
         if self.position[0] % 1 < self.thickness:
             if mapa.isWall(int(self.position[0]) - 1, int(self.position[1]), mapArray):
